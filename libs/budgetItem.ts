f35e733764c0353ae14be8ea9e918c6caef55395
unknown <iam180622@gmail.com>
2025-01-28 21:22:31 +0800

    add budget item function

---
+++ libs/budgetItem.ts
@@ -0,0 +1,27 @@
+import { NewItemData } from "@/types/budgetItemTypes";
+
+export async function AddNewItem(projectID: number, itemData: NewItemData) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/v1/budgetItem/add/${projectID}`,
+      {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(itemData),
+      }
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "Failed to add new item");
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
---
+++ types/budgetItemTypes.ts
@@ -0,0 +1,6 @@
+export interface NewItemData {
+  name: string;
+  amount_allocated: number | 0;
+  description: string;
+  status: string;
+}
