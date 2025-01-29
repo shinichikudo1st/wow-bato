5e591405a22bffd7f6fc5755a95ee7471b94c00c
unknown <iam180622@gmail.com>
2025-01-29 21:45:33 +0800

    get budget item functions

--- libs/budgetItem.ts
+++ libs/budgetItem.ts
@@ -25,3 +25,25 @@ export async function AddNewItem(projectID: number | null, itemData: NewItemData
     throw error;
   }
 }
+
+export async function GetBudgetItemsByProjectID(projectID: number | null, filter: string) {
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/v1/budgetItem/getByProject/${projectID}?filter=${filter}`,
+      {
+        credentials: "include",
+        method: "GET",
+      }
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.error || "Failed to retrieve budget items");
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
